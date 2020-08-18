4bbc1d3a865dee20782e37460039044fcfa22aa4
Alessandro Nerla <nerla.alessandro@gmail.com>
2020-08-18 18:22:27 +0200

    Updated path in example file

--- Classification/Perceptron/example.py
+++ Classification/Perceptron/example.py
@@ -2,7 +2,7 @@ import pandas as pd
 from Classification.Perceptron.single_layer_perceptron import SingleLayerPerceptron
 from Classification.Perceptron.single_layer_perceptron import Point
 
-perceptron = SingleLayerPerceptron(2, Point.create_point_list(pd.read_csv("points.csv"), "cat"))
+perceptron = SingleLayerPerceptron(2, Point.create_point_list(pd.read_csv("Datasets/points.csv"), "cat"))
 T1 = Point(-1, [1, 3])
 T2 = Point(1, [8, 3])
 perceptron.classify(T1)
